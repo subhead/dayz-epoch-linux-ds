440a9dae307941efc78b1ff278723d8dfbb07368
subhead <subhead@gmail.com>
2015-02-15 20:37:20 +0100

    Loadout and Mods
    Changed the default loadout and added the mission pbo with single
    currency, dynamic survivors, dynamic traders, epoch admin tool and
    custom welcome message.

---
+++ restart/config.php
@@ -0,0 +1,6 @@
+<?php
+// Change make sure battleye is enabled and insert your servers information here.
+define("address","0.0.0.0");	// server address
+define("port","2302");	// server port
+define("password","changeme");	// battleye password
+?>
